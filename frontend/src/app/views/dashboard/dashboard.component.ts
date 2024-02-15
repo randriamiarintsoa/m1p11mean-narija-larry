d2b7fccec21e64efdaa76905941a63b5a043880e
randriamiarintsoa <narijamiarintsoa@gmail.com>
2024-02-15 22:29:43 +0300

    ajout list user static

--- frontend/src/app/views/dashboard/dashboard.component.ts
+++ frontend/src/app/views/dashboard/dashboard.component.ts
@@ -3,6 +3,10 @@ import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
 import { DashboardChartsData, IChartProps } from './dashboard-charts-data';
 
+import { UserService } from 'src/app/shared/services/user.service';
+import { User } from 'src/app/shared/models/user.model';
+import { ListResult, ListPagination } from 'src/app/shared/models/list.interface';
+
 interface IUser {
   name: string;
   state: string;
@@ -22,7 +26,24 @@ interface IUser {
   styleUrls: ['dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor(private chartsData: DashboardChartsData) {
+
+  dataSource!: ListResult<User>;
+  user: User = new User();
+  cle!: string;
+  objectUser: any = {};
+  listing: ListPagination = {
+    limit: 10,
+    page: 1,
+    total: 0,
+    pageSizeOptions: [2, 5, 10, 25, 100]
+};
+isLoading: boolean;
+
+  constructor(
+    private chartsData: DashboardChartsData,
+    private userService: UserService,
+    ) {
+      this.isLoading = false;
   }
 
   public users: IUser[] = [
@@ -113,8 +134,25 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.initCharts();
+    this.loadData();
   }
-
+  async loadData() {
+    try {
+        this.isLoading = true;
+        const query: any =  {};
+        query.sort = [{
+          key : 'createdAt',
+          ascendant: false
+         }]
+        this.dataSource = await this.userService.list(this.listing.page, this.listing.limit, query);
+        console.log ('trdddd' ,this.dataSource )
+        this.listing.total = this.dataSource.total;
+        this.isLoading = false;
+    } catch (e) {
+        console.error(e);
+        this.isLoading = false;
+    }
+}
   initCharts(): void {
     this.mainChart = this.chartsData.mainChart;
   }
