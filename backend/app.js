4db195ac4b1602ec86e06075779a7216a8a40e1c
randriamiarintsoa <narijamiarintsoa@gmail.com>
2024-02-26 09:19:56 +0300

    Fix

--- backend/app.js
+++ backend/app.js
@@ -178,7 +178,7 @@ app.delete('/user/:id', async (req, res) => {
 });
   
 
-  // Endpoint pour envoyer un e-mail
+// Endpoint pour envoyer un e-mail
 app.post('/send-email', (req, res) => {
     // Récupérer les informations du formulaire
     const { to, subject, text } = req.body;
