29bb5b1458776f623005fed922bdf72e7bca7efb
randriamiarintsoa <narijamiarintsoa@gmail.com>
2024-02-28 09:38:21 +0300

    fix env frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     // apiUrl: 'http://localhost:3000'
     // apiUrl: 'https://meanapi-38c17.web.app'
-    apiUrl: 'https://m1p11mean-narija-larry.onrender.com/'
+    apiUrl: 'https://m1p11mean-narija-larry.onrender.com'
     
   };
   
